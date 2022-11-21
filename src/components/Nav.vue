4f30feb2d4b1ec356cbbe95886a827dd94fa9004
Risqifaiz <risqifaisal1000@gmail.com>
2022-11-21 20:50:44 +0700

    login

---
+++ src/components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="navbar navbar-expand navbar-light fixed-top">
+        <a href="#" class="navbar-brand">Home</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="navbar-item">
+                    <a href="#" class="nav-link">Login</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="#" class="nav-link">Sign Up</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Nav'
+}
+</script>
---
+++ src/components/Register.vue
@@ -0,0 +1,72 @@
+<template>
+ <form @submit.prevent="hendleSubmit">
+        <h3>Register</h3>
+
+        <div class="form-group">
+            <label>First Name</label>
+            <input type="text" class="form-control" v-model="first_name" placeholder="First Name" />
+        </div>
+
+        <div class="form-group">
+            <label>Last Name</label>
+            <input type="text" class="form-control"  v-model="last_name" placeholder="Last Name" />
+        </div>
+
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" class="form-control"  v-model="email" placeholder="Email" />
+        </div>
+
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" v-model="password" placeholder="Password" />
+        </div>
+
+        <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="password" class="form-control" v-model="confirm_password" placeholder="ConfirmPassword" />
+        </div>
+
+        <button class="btn btn-primary btn-block">Sign Up</button>
+    </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Register',
+    data() {
+        return {
+            first_name:'',
+            last_name:'',
+            email:'',
+            password:'',
+            confirm_password: '',
+        }
+    },
+    methods: {
+      async hendleSubmit() {
+        const response = await axios.post('register', {
+            first_name: this.first_name,
+            last_name: this.last_name,
+            email: this.email,
+            password: this.password,
+            confirm_password: this.confirm_password
+       },[]);
+
+       console.log(response);
+    //    this.$router.push('/login')
+        // .then(
+        //     res => {
+        //         console.log(res)
+        //     }
+        // ).catch(
+        //     err => {
+        //         console.log(err)
+        //     }
+        // )
+        }
+    }
+}
+</script>
